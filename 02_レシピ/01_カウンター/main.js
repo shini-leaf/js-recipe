66b831f0791a591212cae3c61d0132c88d012540
shini-leaf <shiniyou137@gmail.com>
2022-07-05 00:25:24 +0900

    [3の倍数]を学習しました。

---
+++ 02_レシピ/01_カウンター/main.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+const genkiFunction = (number) => {
+  for (let n = 1; n <= number; n++) {
+    if (n % 3 === 0) {
+      console.log(`${n}!!!!!!!`)
+    } else {
+      console.log(n)
+    }
+  }
+}
